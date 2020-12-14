0e4b63cbe718448f01e739a495c05f9ea5e0099d
Stanislav Glushchenko <Stanislav.Glushchenko@emprevo.com>
2020-12-14 23:15:35 +1100

    auto scaffold GameEventsController

---
+++ GameInsightsApp/Controllers/GameEventsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GameInsightsApp.Models;
+using GameInsightsApp.Models.Entities;
+
+namespace GameInsightsApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameEventsController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public GameEventsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/GameEvents
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GameEvent>>> GetGameEvents()
+        {
+            return await context.GameEvents.ToListAsync();
+        }
+
+        // GET: api/GameEvents/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GameEvent>> GetGameEvent(int id)
+        {
+            var gameEvent = await context.GameEvents.FindAsync(id);
+
+            if (gameEvent == null)
+            {
+                return NotFound();
+            }
+
+            return gameEvent;
+        }
+
+        // PUT: api/GameEvents/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGameEvent(int id, GameEvent gameEvent)
+        {
+            if (id != gameEvent.Id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(gameEvent).State = EntityState.Modified;
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GameEventExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/GameEvents
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<GameEvent>> PostGameEvent(GameEvent gameEvent)
+        {
+            context.GameEvents.Add(gameEvent);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGameEvent", new { id = gameEvent.Id }, gameEvent);
+        }
+
+        // DELETE: api/GameEvents/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGameEvent(int id)
+        {
+            var gameEvent = await context.GameEvents.FindAsync(id);
+            if (gameEvent == null)
+            {
+                return NotFound();
+            }
+
+            context.GameEvents.Remove(gameEvent);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GameEventExists(int id)
+        {
+            return context.GameEvents.Any(e => e.Id == id);
+        }
+    }
+}
